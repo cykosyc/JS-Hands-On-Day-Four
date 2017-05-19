bb16bca1252d760165c33f0d236776ef85fa6baf
cykosyc <michael.a.aguilera@gmail.com>
2017-05-19 16:35:52 -0700

    added

---
+++ jsDayfour.js
@@ -0,0 +1,36 @@
+/*
+Now that you have more of an understanding of JavaScript we will be using these in this project project to solidify these concepts through hands-on, practical use. Hands-On projects are completed outside of the browser, using your IDE, or editor of choice.
+
+Description:
+In this project, we will be building a complex function that will allow us to generate numbers. Our function will need to take the two previous number and add them together to get the next number in our sequence. For Example, 1, 2, 3, 5, 8, 13.
+
+Requirements
+An executable function that can print at least 20 numbers in the correct sequence.
+
+Bonus Display the information in the window instead of the console.
+
+Grading
+Meets all Requirements: 50% of your grade will be based on meeting the requirements.
+Timely Submission: 25% of your grade will be based on having a complete solution on time.
+Style: 25% of your grade will be based on having legible, and well-designed code.
+*/
+
+// function alpha(a) {
+//   for (var i = a; a < 20; i++){
+//     console.log(i);
+//   }
+// }
+// alpha(1);
+//
+
+for (var i = 1, t = 1; i < 21; i++){
+  console.log(i);
+}
+
+// var numbers = [1, 2]
+// for (var i = 0; i < numbers.length; i++){
+//   if (numbers.length != 20){
+//     numbers.push (20) + 2;
+//   }
+//   console.log(i+1);
+// }
